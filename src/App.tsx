60013af90f0e6b943a6bf2f92dc959f486db60de
Swiichy <adlpromail@gmail.com>
2023-03-19 00:20:12 +0100

    feat: add information message

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,25 @@ import { MainLayout } from "./components/layouts/MainLayout";
 import { Header } from "./components/Header";
 import { FormGroup } from "./components/FormGroup";
 import { DataList } from "./components/DataList/DataList";
+import { Typography } from "@mui/material";
 
 export const App = () => {
   return (
     <AppLayout>
       <Header />
       <MainLayout>
+        <Typography sx={{ textAlign: "center", maxWidth: "500px" }}>
+          Nous travaillons activement pour améliorer et mettre à jour
+          régulièrement l'application afin de vous offrir la meilleure
+          expérience possible. Veuillez noter que les données sont actuellement
+          stockées dans votre navigateur.
+          <Typography sx={{ color: "red" }}>
+            Afin de ne pas perdre vos données, veillez à ne pas réinitialiser
+            votre navigateur ni supprimer le localStorage.
+          </Typography>
+          Merci de votre compréhension et n'hésitez pas à nous faire part de vos
+          suggestions et commentaires.
+        </Typography>
         <FormGroup />
         <DataList />
       </MainLayout>
