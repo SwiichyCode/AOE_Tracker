a34740cd08347d3566a4485a03f7855916c43d56
Swiichy <adlpromail@gmail.com>
2023-03-20 13:31:42 +0100

    update: fix message

--- src/App.tsx
+++ src/App.tsx
@@ -28,11 +28,8 @@ export const App = () => {
               </Typography>
             </Alert>
             <Alert severity="info">
-              Vous pouvez modifier la cellule (Name, Prix d'achat, Prix de
-              vente), mais il y a encore quelques problèmes à résoudre. Pour
-              enregistrer la modification, éditez la cellule, sortez de la
-              cellule et cliquez à nouveau pour sauvegarder les modifications
-              effectuées.
+              Fix des modifications des cellules ! Cela fonctionne correctement
+              maintenant.
             </Alert>
           </Box>
           <DataList />
